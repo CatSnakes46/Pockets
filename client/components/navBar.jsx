573002efadd25cf32a9cfe253adc50d339a4a07a
Kenneth Lee <87886047+kennyea@users.noreply.github.com>
2021-10-19 20:06:03 -0700

    created functions for handling login/logout. created buttons

--- client/components/navBar.jsx
+++ client/components/navBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const NavBar = () => {
+  const handleLogin = () => {
+    console.log('yayyy logging in');
+  };
+
+  const handleLogout = () => {
+    console.log('yayyyy logging out');
+  };
+
   return (
-      <div>
-      </div>
-  )
-}
+    <div>
+      <button className="log" onClick={handleLogin}>
+        Login
+      </button>
+      <button className="log" onClick={handleLogout}>
+        Log Out
+      </button>
+    </div>
+  );
+};
 
 export default NavBar;
